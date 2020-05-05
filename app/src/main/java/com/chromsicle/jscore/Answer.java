64f885fc6c5407238cb94b8e02049b29ea7b20e0
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-05-05 14:47:57 -0700

    notes about code improvement added

--- app/src/main/java/com/chromsicle/jscore/Answer.java
+++ app/src/main/java/com/chromsicle/jscore/Answer.java
@@ -9,23 +9,66 @@ import android.widget.TextView;
 
 public class Answer extends AppCompatActivity {
 
+    int clueAmount;
+    int score;
+    boolean doubleRound;
+
+    public static final String EXTRA_SCORE = "com.chromsicle.application.jscore.EXTRA_SCORE";
+    public static final String EXTRA_ROUND = "com.chromsicle.application.jscore.EXTRA_ROUND";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
 
+        //get the clue amount, score, and round from MainActivity
         Intent intent = getIntent();
-        int number = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+        clueAmount = intent.getIntExtra(MainActivity.EXTRA_CLUE, 0);
+        score = intent.getIntExtra(MainActivity.EXTRA_SCORE, 0);
+        doubleRound = intent.getBooleanExtra(MainActivity.EXTRA_ROUND, false);
 
+        //set the $ TextView to be the clue amount that was selected
         TextView clueAmountTextView = findViewById(R.id.clueAmount);
-        clueAmountTextView.setText("$" + number);
+        clueAmountTextView.setText("$" + clueAmount);
 
+        //get the back, correct, incorrect, and pass buttons
         Button backButton = findViewById(R.id.backButton);
+        Button correctButton = findViewById(R.id.correctButton);
+        Button incorrectButton = findViewById(R.id.incorrectButton);
+        Button passButton = findViewById(R.id.passButton);
 
     }
 
+    //when back button pressed, go to MainActivity with the current game state and score
     public void backButtonClicked(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    //when correct button pressed, add clueAmount to the score then send back to MainActivity
+    public void correctButtonClicked(View v) {
+        score += clueAmount;
+        openMainActivity();
+    }
+
+    //when incorrect button pressed, deduct clueAmount to the score then send back to MainActivity
+    public void incorrectButtonClicked (View v) {
+        score -= clueAmount;
+        openMainActivity();
+    }
+
+    //when passed button pressed, send back to MainActivity
+    public void passButtonClicked(View v) {
+        openMainActivity();
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent();
+        //Intent intent = new Intent(this, MainActivity.class);
+        //open MainActivity with the score and round
+        intent.putExtra(EXTRA_SCORE, score);
+        intent.putExtra(EXTRA_ROUND, doubleRound);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/chromsicle/jscore/MainActivity.java
+++ app/src/main/java/com/chromsicle/jscore/MainActivity.java
@@ -2,36 +2,45 @@ package com.chromsicle.jscore;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+
+//left off by trying to figure out how to get the score and double status back from the second activity
+//https://www.youtube.com/watch?v=AD5qt7xoUU8 might help
+//is this something that needs to be done with shared preferences instead?
+//why can't I send data back and forth the way I send it to Answers in the first place?
+
+
+/**
+ * right now the program uses the onclick in the xml but this should be changed to use setOnClickListener
+ * like in the miwok app
+ * also, learn how those todo things work (freaky, everything turned yellow after writing todo)
+ */
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static int score = 0;
-    public static boolean doubleVaule = false;
-    int correct200;
-    int incorrect200;
-    int correct400;
-    int incorrect400;
-    int correct600;
-    int incorrect600;
-    int correct800;
-    int incorrect800;
-    int correct1000;
-    int incorrect1000;
-    int correctDouble400;
-    int incorrectDouble400;
+    public int score;
+    public boolean doubleValue = false;
 
-    int round;
+    Button clue1;
+    Button clue2;
+    Button clue3;
+    Button clue4;
+    Button clue5;
 
     int clueAmount;
 
 
-    //constant for the passed int, best practice is to use package name so interacting with other apps doesn't lead to issues
-    public static final String EXTRA_NUMBER = "com.chromsicle.application,jscore.EXTRA_NUMBER";
+    //constant for the passed ints, best practice is to use package name so interacting with other apps doesn't lead to issues
+    public static final String EXTRA_CLUE = "com.chromsicle.application.jscore.EXTRA_CLUE";
+    public static final String EXTRA_SCORE = "com.chromsicle.application.jscore.EXTRA_SCORE";
+    //this doesn't seem like the right way to deal with this since it'll never change in the Answer activity so research a better way
+    public static final String EXTRA_ROUND = "com.chromsicle.application.jscore.EXTRA_ROUND";
 
 
     @Override
@@ -39,14 +48,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //set the starting round
-        round = 1;
-
-        Button clue1 = findViewById(R.id.clue1);
-        Button clue2 = findViewById(R.id.clue2);
-        Button clue3 = findViewById(R.id.clue3);
-        Button clue4 = findViewById(R.id.clue4);
-        Button clue5 = findViewById(R.id.clue5);
+        //get the buttons
+        clue1 = findViewById(R.id.clue1);
+        clue2 = findViewById(R.id.clue2);
+        clue3 = findViewById(R.id.clue3);
+        clue4 = findViewById(R.id.clue4);
+        clue5 = findViewById(R.id.clue5);
 
         //clicks sent to the onClick method
         clue1.setOnClickListener(this);
@@ -56,204 +63,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clue5.setOnClickListener(this);
     }
 
-    public void displayScore(int money){
-        TextView scoreView = findViewById(R.id.winnings);
-        scoreView.setText("$" + money);
-    }
-
-
-    public void addPoints(View v) {
-        v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
-        v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-        switch(v.getId()) {
-            case R.id.clue1:
-                if(doubleVaule) {
-                    score += 400;
-                }
-                else {
-                    score += 200;
-                }
-                displayScore(score);
-                break;
-            case R.id.clue2:
-                if(doubleVaule) {
-                    score += 800;
-                }
-                else {
-                    score += 400;
-                }
-                displayScore(score);
-                break;
-            case R.id.clue3:
-                if(doubleVaule) {
-                    score += 1200;
-                }
-                else {
-                    score += 600;
-                }
-                displayScore(score);
-                break;
-            case R.id.clue4:
-                if(doubleVaule) {
-                    score += 1600;
-                }
-                else {
-                    score += 800;
-                }
-                displayScore(score);
-                break;
-            case R.id.clue5:
-                if(doubleVaule) {
-                    score += 2000;
-                }
-                else {
-                    score += 1000;
-                }
-                displayScore(score);
-                break;
-            default:
-                throw new RuntimeException("Unknown button ID");
-        }
-    }
-
-    /*
-    public void deductPoints(View v) {
-        v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
-        v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-        switch(v.getId()) {
-            case R.id.negFirst:
-                if(doubleVaule) {
-                    score -= 400;
-                }
-                else {
-                    score -= 200;
-                }
-                displayScore(score);
-                break;
-            case R.id.negSecond:
-                if(doubleVaule) {
-                    score -= 800;
-                }
-                else {
-                    score -= 400;
-                }
-                displayScore(score);
-                break;
-            case R.id.negThird:
-                if(doubleVaule) {
-                    score -= 1200;
-                }
-                else {
-                    score -= 600;
-                }
-                displayScore(score);
-                break;
-            case R.id.negFourth:
-                if(doubleVaule) {
-                    score -= 1600;
-                }
-                else {
-                    score -= 800;
-                }
-                displayScore(score);
-                break;
-            case R.id.negFifth:
-                if(doubleVaule) {
-                    score -= 2000;
-                }
-                else {
-                    score -= 1000;
-                }
-                displayScore(score);
-                break;
-            default:
-                throw new RuntimeException("Unknown button ID");
-        }
-    }
-
-     */
-
-    public void doubleRound (View view) {
-        view.playSoundEffect(android.view.SoundEffectConstants.CLICK);
-        view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-
-        doubleVaule = true;
-
-        /*firstButton = findViewById(R.id.clue1);
-        firstButton.setText("$400");
-
-        secondButton = findViewById(R.id.clue2);
-        secondButton.setText("$800");
-
-        thirdButton = findViewById(R.id.clue3);
-        thirdButton.setText("$1200");
-
-        fourthButton = findViewById(R.id.clue4);
-        fourthButton.setText("$1600");
-
-        fifthButton = findViewById(R.id.clue5);
-        fifthButton.setText("$2000");
-*/
-        /*Button negFirstButton = findViewById(R.id.negFirst);
-        negFirstButton.setText("-$400");
-
-        Button negSecondButton = findViewById(R.id.negSecond);
-        negSecondButton.setText("-$800");
-
-        Button negThirdButton = findViewById(R.id.negThird);
-        negThirdButton.setText("-$1200");
-
-        Button negFourthButton = findViewById(R.id.negFourth);
-        negFourthButton.setText("-$1600");
-
-        Button negFifthButton = findViewById(R.id.negFifth);
-        negFifthButton.setText("-$2000");*/
-    }
-
     @Override
     public void onClick(View view) {
         //which button was clicked?
         switch (view.getId()) {
             case R.id.clue1:
-                //what I want to happen (send the $ to the next activity and open it so that the amount will be added or subtracted form the total)
-                if(round == 1) {
-                    clueAmount = 200;
-                } else if (round == 2) {
+                //set the chosen clue amount
+                if(doubleValue) {
                     clueAmount = 400;
+                } else {
+                    clueAmount = 200;
                 }
-                //convert the amount to a string
-                //String clueAmountString = String.valueOf(clueAmount);
                 openAnswerActivity();
                 break;
             case R.id.clue2:
-                if(round == 1) {
-                    clueAmount = 400;
-                } else if (round == 2) {
+                if(doubleValue) {
                     clueAmount = 800;
+                } else {
+                    clueAmount = 400;
                 }
                 openAnswerActivity();
                 break;
             case R.id.clue3:
-                if(round == 1) {
-                    clueAmount = 600;
-                } else if (round == 2) {
+                if(doubleValue) {
                     clueAmount = 1200;
+                } else {
+                    clueAmount = 600;
                 }
                 openAnswerActivity();
                 break;
             case R.id.clue4:
-                if(round == 1) {
-                    clueAmount = 800;
-                } else if (round == 2) {
+                if(doubleValue) {
                     clueAmount = 1600;
+                } else {
+                    clueAmount = 800;
                 }
                 openAnswerActivity();
                 break;
             case R.id.clue5:
-                if(round == 1) {
-                    clueAmount = 1000;
-                } else if (round == 2) {
+                if(doubleValue) {
                     clueAmount = 2000;
+                } else {
+                    clueAmount = 1000;
                 }
                 openAnswerActivity();
                 break;
@@ -263,7 +114,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void openAnswerActivity() {
         Intent intent = new Intent(this, Answer.class);
-        intent.putExtra(EXTRA_NUMBER, clueAmount);
-        startActivity(intent);
+        //open the Answer activity with the selected clueAmount and current score
+        //should these and the ones coming from the Answer activity be passed as a bundle?
+        intent.putExtra(EXTRA_CLUE, clueAmount);
+        intent.putExtra(EXTRA_SCORE, score);
+        intent.putExtra(EXTRA_ROUND, doubleValue);
+        startActivityForResult(intent, 1);
+    }
+
+    public void doubleRound(View view) {
+        //set round to 2, update clue TextViews
+        doubleValue = true;
+
+        clue1.setText("$400");
+        clue2.setText("$800");
+        clue3.setText("$1200");
+        clue4.setText("$1600");
+        clue5.setText("$2000");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                int result = data.getIntExtra("EXTRA_SCORE", 0);
+                TextView scoreTextView = findViewById(R.id.winnings);
+                scoreTextView.setText("$" + result);
+            }
+        }
     }
 }
